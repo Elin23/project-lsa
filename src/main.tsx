46e469d646b3a7953d279ef7ebd0e0dfa54b446f
Mustafa Rashid <mustafa.rashid.abbas@gmail.com>
2026-06-19 20:11:52 +0300

    feat: add create create browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout.tsx'
+import HomePage from './pages/HomePage.tsx'
+import CareersPage from './pages/CareersPage.tsx'
+import ContactPage from './pages/ContactPage.tsx'
+import AboutPage from './pages/AboutPage.tsx'
+import EngineeringServicesPage from './pages/EngineeringServicesPage.tsx'
+import ProjectPage from './pages/ProjectPage.tsx'
+import ProjectDetailsPage from './pages/ProjectDetails.tsx'
+import EngineeringServiceDeatilsPage from './pages/EngineeringServiceDeatilsPage.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "about", element: <AboutPage /> },
+      { path: "engineering-services", element: <EngineeringServicesPage /> },
+      { path: "engineering-services/:id", element: <EngineeringServiceDeatilsPage /> },
+      { path: "projects", element: <ProjectPage /> },
+      { path: "projects/:id", element: <ProjectDetailsPage /> },
+      { path: "careers", element: <CareersPage /> },
+      { path: "contact", element: <ContactPage /> },
+    ],
+  },
+])
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
