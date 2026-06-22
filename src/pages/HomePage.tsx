5ccc576595eeb0d9fa40be5475d4b50e986add68
Elin23 <elinshaia23@gmail.com>
2026-06-22 16:27:32 +0300

    OurCoreCapabilities

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import CTASection from "../components/shared/CTA/Cta"
 import Testimonials from "../sections/Home/Testimonials"
 import TrustedCompanies from "../sections/Home/TrustedCompanies"
+import OurCoreCapabilities from "../sections/HomePageSections/OurCoreCapabilities/OurCoreCapabilities"
 
 const HomePage = () => {
   return (
     <div className="space-y-12">
       <TrustedCompanies />
+      <OurCoreCapabilities/>
       <Testimonials />
       <CTASection />
     </div>
